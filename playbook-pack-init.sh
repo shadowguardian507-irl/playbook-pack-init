53f9c1d810a2732a8fed6b2cec4be236791fc427
shadowguardian507-irl <shadow@shadowguardian507-irl.uk>
2019-05-19 14:57:58 +0100

    added comand line options for headless running + more azure pipelines stuff

--- playbook-pack-init.sh
+++ playbook-pack-init.sh
@@ -1,14 +1,62 @@
 #!/bin/bash
+AUTORUN=false
+while getopts "yash" opt; do
+
+  case $opt in
+    y) 
+      echo "option y"
+      AUTORUN=true
+    ;;
+    a)
+      echo "option a"
+      AUTORUN=true
+    ;;
+    s)
+      echo "option s"
+      AUTORUN=true
+    ;;
+    h) 
+       echo ''
+       echo ' option flags are'
+       echo ''
+       echo ' -a (auto run)'
+       echo ' -y (yes)'
+       echo ' -s (scripting mode)'
+       echo ' all above options result in no confirm prompts please use with care'
+       echo ''
+       echo ' -h shows this help message' >&2
+       exit 1
+    ;;
+    *) 
+       echo ''
+       echo 'valid option flags are as follows'
+       echo ''
+       echo ' -a (auto run)'
+       echo ' -y (yes)'
+       echo ' -s (scripting mode)'
+       echo ' all above options result in no confirm prompts please use with care'
+       echo ''
+       echo ' -h (help)' >&2
+       exit 1
+  esac
+
+done
+
 
 echo "initialising playbook pack structure in directory $(pwd)"
-while true; do
+if $AUTORUN
+then
+  echo "auto run enabled by flag"
+else
+  while true; do
     read -p "do you wish to continue ? (Y/N) " yn
     case $yn in
         [Yy]* ) echo processing ; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer y or n.";;
     esac
-done
+  done
+fi
 echo "running stage 1"
 mkdir ./Ansible
 mkdir ./Resources
